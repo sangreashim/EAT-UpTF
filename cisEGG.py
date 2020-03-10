ef1081878aa717a16437729f496d0398d03055b7
sangreashim <sangreashim@gmail.com>
2020-03-10 15:17:46 +0900

    first commit

---
+++ cisEGG.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+import sys
+import scipy.stats as st
+from statsmodels.stats.multitest import multipletests as mt
+import argparse
+parser  = argparse.ArgumentParser(description="cistrome Enrichment analysis tool for Group of Genes (cisEGG) : conduct enrichment analysis of cistrome using DAP-seq (O'Malley et al 2016) and provide putative transcription factor for specific gene group of interest")
+parser.add_argument('--cistrome', help='database file of cistrome', type=argparse.FileType('r'))
+parser.add_argument('--gene_group', help='file containing group of genes', type=argparse.FileType('r'))
+parser.add_argument('--num_ref_genes','-N', help='No. of total genes in reference genome, default=27206 for Arabidopsis thaliana', default=27206.0, type=float)
+parser.add_argument('--model', help='statistical model: HG=hypergeometric test, BN=binomial test, default=HG', default='HG', type=str)
+parser.add_argument('--post_hoc', help='post hoc analysis: bonferroni=Bonferroni family-wise error rate correction, fdr_bh=Benjamini & Hochberg False Discover Rate correction, default=fdr_bh', default='fdr_bh', type=str)
+parser.add_argument('--alpha', help='alpha in post hoc analysis, default:0.05', default=0.05, type=float)
+parser.add_argument('--output', help='output file name', default='cisEGG.output', type=argparse.FileType('w'))
+parser.add_argument('--alias', help='gene alias file, default=NA', default='NA', type=str)
+args = parser.parse_args()
+
+def alias_anno(opened_file_list):
+    alias   = {}
+    for gene in opened_file_list:
+        gene_cell   = gene.strip().split('\t')
+        if len(gene_cell)>2:
+            ids = gene_cell[0]
+            sym = gene_cell[1]
+            func    = gene_cell[2]
+        else:
+            ids = gene_cell[0]
+            sym = gene_cell[1]
+            func    = 'NA'
+        if ids in alias.keys():
+            alias[ids][0].append(sym)
+            alias[ids][1].append(func)
+        else:
+            alias[ids]  = [[sym],[func]]
+    return alias
+
+####################################################################################
+
+cis_dict  = {}
+cis   = args.cistrome.readlines() #open(sys.argv[1],'r').xreadlines()
+gene_list   = [x.strip() for x in args.gene_group.readlines()] #open(sys.argv[2],'r').readlines()]
+
+n   = float(len(gene_list))
+N   = args.num_ref_genes #float(sys.argv[3]) # No. of total genes in reference genome
+
+for i in cis:
+    cell    = i.strip().split()
+    tf  = cell[0]
+    target  = cell[1]
+
+    if tf in cis_dict.keys():
+        cis_dict[tf].append(target)
+    else:
+        cis_dict[tf]   = [target]
+
+information_dict    = {}
+id_list = []
+p_value_list    = []
+adjusted_p_value_list   = []
+
+for j in cis_dict.keys():
+    x   = len(list(set(cis_dict[j])))
+    y   = len(set(gene_list).intersection(set(cis_dict[j])))
+    expected = (x/N)
+    if args.model   == 'BN':
+        p_value = st.binom_test(y,n,expected)
+    else:
+        p_value = st.hypergeom.sf(y-1,N,x,n)
+
+    information_dict[j.strip()] = [str(y),str(int(n)),str(y/n),str(x),str(int(N)),str(expected),str(p_value)]
+    id_list.append(j.strip())
+    p_value_list.append(p_value)
+
+adjusted_p_value_list = mt(p_value_list, alpha=args.alpha, method=args.post_hoc)
+
+output  = args.output
+
+if args.alias   == 'NA':
+    output.write('TF (AGI)'+'\t'+ 'x' +'\t'+ 'n' +'\t'+ 'observed (ratio)' +'\t'+ 'X' +'\t'+ 'N' +'\t'+ 'expected (ratio)' +'\t'+ 'p-value' +'\t'+ 'q-value' +'\n')
+    for k in range(len(id_list)):
+        output.write(id_list[k] +'\t'+ '\t'.join(information_dict[id_list[k]]) +'\t'+ str(adjusted_p_value_list[1][k])+'\n')
+else:
+    alias_dict  = alias_anno(open(args.alias,'r').readlines())
+    output.write('TF (AGI)'+'\t'+ 'x' +'\t'+ 'n' +'\t'+ 'observed (ratio)' +'\t'+ 'X' +'\t'+ 'N' +'\t'+ 'expected (ratio)' +'\t'+ 'p-value' +'\t'+ 'q-value' +'\t'+ 'gene symbol' +'\t'+ 'gene name' +'\n')
+    for k in range(len(id_list)):
+        try:
+            output.write(id_list[k] +'\t'+ '\t'.join(information_dict[id_list[k]]) +'\t'+ str(adjusted_p_value_list[1][k])+'\t'+ ', '.join(list(set(alias_dict[id_list[k]][0]))) +'\t'+ ', '.join(list(set(alias_dict[id_list[k]][1]))) +'\n')
+        except KeyError:
+            output.write(id_list[k] +'\t'+ '\t'.join(information_dict[id_list[k]]) +'\t'+ str(adjusted_p_value_list[1][k])+'\t'+ ' ' +'\t'+ ' ' +'\n')
