d53540751a2ded49b4e49fbbd6df4d9380972e69
sangreashim <sangreashim@gmail.com>
2021-04-19 10:42:25 +0900

    second

--- network.py
+++ network.py
@@ -21,7 +21,7 @@ for x in args.cistrome.readlines():
     else:
         cistrome_dic[tf_id].append(target_id)
 
-gene_set   = set([x.strip() for x in args.gene_group.readlines()])
+gene_set   = set([x.strip().upper() for x in args.gene_group.readlines()])
 gene_stack  = []
 args.output.write('Source'+'\t'+'InteractionType'+'\t'+'Target'+'\t'+'Val'+'\n')
 for tf in [x.strip() for x in args.TFs.readlines()]:
